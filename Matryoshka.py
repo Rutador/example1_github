c6ebde48f9ce2e1fd749e53ee7f7d2f7f92a5f07
rutador <rutador@gmail.com>
2020-06-12 13:36:33 +0300

    added .py file

---
+++ Matryoshka.py
@@ -0,0 +1,11 @@
+def matryoshka(n):
+    """ Рекурсия в матрешке
+    """
+    if n == 1:
+        print("Матрешечка")
+    else:
+        print("Верх матрешки n =", n)
+        matryoshka(n - 1)
+        print("Низ матрешки n =", n)
+
+matryoshka(5)
